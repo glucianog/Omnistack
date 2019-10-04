da3c2df8e36476b93c0808c9a27fee77ccbe8d1f
glucianog <glgomes@sga.pucminas.br>
2019-10-04 00:15:02 -0300

    Mobile completo

---
+++ mobile/src/pages/List.js
@@ -0,0 +1,45 @@
+import React , { useState, useEffect } from 'react';
+import {AsyncStorage, Image, SafeAreaView, ScrollView, StyleSheet} from 'react-native';
+
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png';
+
+
+
+export default function List() {
+    const [techs, setTechs] = useState([]);
+
+    useEffect(()=>{
+        AsyncStorage.getItem('techs').then(storagedTechs => {
+            const techArray = storagedTechs.split(',').map(tech => tech.trim());
+
+            setTechs(techArray);
+        })
+
+    }, []);
+    return (
+        <SafeAreaView style={styles.container}>
+            <Image style={styles.logo} source={logo} />
+            <ScrollView>
+                {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+            </ScrollView>
+            
+        </SafeAreaView>
+    )
+    
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+    },
+
+    logo:{
+        height: 32,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+        marginTop: 10,        
+    },
+
+}); 
