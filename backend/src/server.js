f46c27e76160a3bd9fb104f1c25dae5c05eb8cf4
glucianog <glgomes@sga.pucminas.br>
2019-10-04 20:53:13 -0300

    Realtime communication

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,15 +3,35 @@ const mongoose = require('mongoose')
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes  = require('./routes');
 
-
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-6ezzu.mongodb.net/aulaweb?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
+
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next(); // possibilita a continuação do fluxo do código
+})
 
 // req.query -> acessar query params (filtros)
 // req.params -> acessar route params (edição e delete)
@@ -22,4 +42,4 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(8000);
+server.listen(8000);
--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,5 +1,6 @@
 import React , { useState, useEffect } from 'react';
-import {AsyncStorage, Image, SafeAreaView, ScrollView, StyleSheet} from 'react-native';
+import socketio from 'socket.io-client';
+import {Alert, AsyncStorage, Image, SafeAreaView, ScrollView, StyleSheet} from 'react-native';
 
 import SpotList from '../components/SpotList';
 
@@ -9,6 +10,18 @@ import logo from '../assets/logo.png';
 
 export default function List() {
     const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user_id => {
+            const socket = socketio('http://192.168.25.2:8000', {
+                query: { user_id }
+            })
+
+            socket.on('booking_response', booking => {
+                Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approved ? 'APROVADA' : 'REJEITADA'}`)
+            })
+        })
+    }, []);
 
     useEffect(()=>{
         AsyncStorage.getItem('techs').then(storagedTechs => {
